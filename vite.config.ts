be080e6cb14b550c1962650599fb3d2ab90ea2e4
laxmanpokhrel <laxmanpokhrel102@gmail.com>
2023-04-13 16:37:08 +0545

    source map added to vite vonfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   resolve: {
+    extensions: [".ts", ".tsx"],
     alias: {
       "@": new URL("./src/", import.meta.url).pathname,
       "@UI": new URL("./src/ui/", import.meta.url).pathname,
@@ -17,5 +18,8 @@ export default defineConfig({
       "@Interfaces": new URL("./src/interfaces/", import.meta.url).pathname,
       "@Hooks": new URL("./src/hooks/", import.meta.url).pathname,
     },
+  },
+  build: {
+    sourcemap: true,
   },
 });
