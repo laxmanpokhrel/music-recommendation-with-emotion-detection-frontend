062452d36d3b8c27fa572dec2c01a9201531f8d0
laxmanpokhrel <laxmanpokhrel102@gmail.com>
2023-05-11 20:26:25 +0545

    delete: files cleanup

--- src/ui/pages/ProjectPage.tsx
+++
@@ -1,9 +0,0 @@
-import ProjectTemplate from "@Templates/ProjectTemplate";
-
-export default function ProjectPage(): JSX.Element {
-  return (
-    <div>
-      <ProjectTemplate />
-    </div>
-  );
-}
