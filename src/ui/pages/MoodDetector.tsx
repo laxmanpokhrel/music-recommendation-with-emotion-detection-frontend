5bda63739e4df6dd72933b30bd4916932faae260
laxmanpokhrel <laxmanpokhrel102@gmail.com>
2023-06-26 19:35:24 +0545

    feat@Sidebar menu: menu navigators added

--- src/ui/pages/MoodDetector.tsx
+++ src/ui/pages/MoodDetector.tsx
@@ -1,22 +1,29 @@
-import { Button } from 'react-day-picker';
+/* eslint-disable react/no-array-index-key */
+import { Button } from '@Atoms/radixComponents/Button';
+import { useNavigate } from 'react-router-dom';
 
 export default function MooDetector() {
+  const navigate = useNavigate();
   return (
-    <div className="laxutw-w-full  laxutw-bg-green-400 laxutw-h-[80vh] laxutw-flex laxutw-gap-4 ">
-      <div className="camera laxutw-bg-red-100 laxutw-flex-2">
+    <div className="laxutw-w-full  laxutw-h-[80vh] laxutw-flex laxutw-gap-8 laxutw-p-4">
+      <div className="laxutw-flex-2">
         <iframe
           title="camera"
           className="laxutw-w-full laxutw-h-full"
           src="https://www.youtube.com/embed/tgbNymZ7vqY"
-          // height="500px"
-          // width="300px"
         />
       </div>
-      <div className="songs laxutw-flex-1">
-        {Array.from({ length: 9 }).map(() => (
-          <div>This is a song</div>
+      <div className="songs laxutw-flex-1 laxutw-flex laxutw-flex-col laxutw-gap-4 laxutw-items-start laxutw-justify-start">
+        {Array.from({ length: 9 }).map((__item, index) => (
+          <div key={index}>This is a song</div>
         ))}
-        <Button>See More</Button>
+        <Button
+          variant="link"
+          className="!laxutw-px-4 !laxutw-py-0 !laxutw-p-0"
+          onClick={() => navigate('/explore-more')}
+        >
+          See More
+        </Button>
       </div>
     </div>
   );
