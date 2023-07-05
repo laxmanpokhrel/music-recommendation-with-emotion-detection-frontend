56eab23fb95b8b92b6ec77bbbd2c9a51f6c6e80e
laxmanpokhrel <laxmanpokhrel102@gmail.com>
2023-07-05 23:51:05 +0545

    feat: upload platlist

--- src/ui/pages/YourPlaylist.tsx
+++ src/ui/pages/YourPlaylist.tsx
@@ -3,11 +3,13 @@ import MusicCard from '@Atoms/MusicCard';
 import { Button } from '@Atoms/radixComponents/Button';
 import { useNavigate } from 'react-router-dom';
 import { PlaylistCreateService } from '../_lib_';
+import Icon from '@Atoms/Icon';
 
 export default function YourPlaylist() {
   const { data } = PlaylistCreateService.fetchData();
   const navigate = useNavigate();
-  if (!data?.length)
+  console.log(data?.data, '---data');
+  if (!data?.data?.length)
     return (
       <div className="w-full flex flex-col justify-center items-center gap-4 h-[92vh]">
         <p className="text-2xl text-gray-600">No Playlist created Yet</p>
@@ -16,22 +18,20 @@ export default function YourPlaylist() {
         </Button>
       </div>
     );
+
   return (
     <div className="home-page-template text-lg w-full h-full grid pt-10">
       <div className="grid grid-cols-3 gap-6 w-3/5 m-auto">
-        {data?.data.map((item: any) => {
+        {data?.data?.map((item: any) => {
           return (
-            <div key={item.title} className="group transition-all duration-150 ease-in rounded-lg overflow-hidden">
-              <Card
-                className="bg-gray-950 group-hover:scale-[1.05] transition-all duration-150 ease-in"
-                key={item.title}
-              >
-                <MusicCard
-                  image={item.media.find((med: Record<string, any>) => med.type === 'THUMBNAIL').path}
-                  title={item.title}
-                  description={item.genre}
-                />
-              </Card>
+            <div className="bg-gray-200 p-2 rounded-lg shadow-xl group min-w-[10rem] border border-gray-300">
+              <h4>{item.name}</h4>
+              {item.musics.map((itm: any) => (
+                <h6 className="pl-4 flex gap-1 w-fit border p-2 hover:bg-gray-400 items-center justify-center">
+                  <p className="flex-1">{itm.title}</p>
+                  <Icon iconName="play_arrow" className="cursor-pointer" />
+                </h6>
+              ))}
             </div>
           );
         })}
